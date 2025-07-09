0ce55ec075c0a22cd763c3eb3cb93fe7473843e5
nishidubey09 <nishidubey@gmail.com>
2025-07-09 17:09:24 +0530

    done

--- javascript/arrtrans.js
+++ javascript/arrtrans.js
@@ -9,6 +9,24 @@ for(let i of arr){
 }
 //for each loop
 arr.forEach((a, b, c) =>{
-    console.log(a,b,c);
+    //console.log(a,b,c);
     //a will guve oaramenter b will give index and c will return all the arr 
 })
+
+const sqr = [];
+arr.forEach((e) =>{
+    sqr.push(e*e);
+})
+console.log(sqr);
+//even
+const even = [];
+arr.forEach((e) => {
+    if(e % 2 == 0) {
+        even.push(e);
+    }
+}) 
+console.log(even);
+
+const prices = [850,205,1260,500,80]
+//find the sum of all prices
+//store pricess greater than 500 in a new array
